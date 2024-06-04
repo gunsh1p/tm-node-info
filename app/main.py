3015ecbfcacd0fc6dd66d30fce4eddc784447f1e
gunsh1p <bertollo.victor@gmail.com>
2024-06-04 19:30:48 +0300

    Changes

--- app/main.py
+++ app/main.py
@@ -2,30 +2,21 @@ import socket
 import json
 from info import get_cpu_percent, get_memory_usage, get_connections
 
-def server_program():
-    port = 10100
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('0.0.0.0', 10100))
+serversocket.listen(5)
 
-    server_socket = socket.socket()
-    server_socket.bind(('0.0.0.0', port))
-
-    server_socket.listen(10)
-    while True:
-        conn, address = server_socket.accept()
-        print(f'Connection from {address}')
-        while True:
-            data = conn.recv(1024)
-            if not data or data != 0x1:
-                break
-            cpu = get_cpu_percent()
-            mem = get_memory_usage()
-            conns = get_connections()
-            send_data = {
-                'cpu': cpu,
-                'memory': mem,
-                'conns': conns
-            }
-            conn.send(json.dumps(send_data).encode())
-        conn.close()
-
-if __name__ == '__main__':
-    server_program()
+while True:
+    connection, address = serversocket.accept()
+    buf = connection.recv(1024)
+    if len(buf) > 0 and buf == 0x1:
+        cpu = get_cpu_percent()
+        mem = get_memory_usage()
+        conns = get_connections()
+        send_data = {
+            'cpu': cpu,
+            'memory': mem,
+            'conns': conns
+        }
+        connection.send(json.dumps(send_data).encode())
+    connection.close()
