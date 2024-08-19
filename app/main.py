e88b602b479ef687b0159a8559184a2f5444e9ae
gunsh1p <bertollo.victor@gmail.com>
2024-08-19 03:14:40 +0300

    Add reboot feature

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import socket
 import json
 from info import get_cpu_percent, get_memory_usage, get_network_usage
+import reboot
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('0.0.0.0', 10100))
+serversocket.bind(("0.0.0.0", 10100))
 serversocket.listen(5)
 
 while True:
@@ -14,17 +15,16 @@ while True:
         continue
     try:
         buf = buf.decode()
-        assert buf == 'data'
+        assert buf == "data" or buf == "reboot"
     except Exception:
         connection.close()
         continue
+    if buf == "reboot":
+        connection.close()
+        reboot.reboot()
     cpu = get_cpu_percent()
     mem = get_memory_usage()
     net = get_network_usage()
-    send_data = {
-        'cpu': cpu,
-        'memory': mem,
-        'network': net
-    }
+    send_data = {"cpu": cpu, "memory": mem, "network": net}
     connection.send(json.dumps(send_data).encode())
     connection.close()
